9c276b7f1c8fcf2824fcd3749b4f8e5259f68d31
Elizabeth Keyes <liz4momentum@gmail.com>
2023-03-10 12:00:31 -0700

    Got basic MVC set up, removed unnecessary docs

--- app/AppState.js
+++ app/AppState.js
@@ -1,11 +1,11 @@
-import { Value } from "./Models/Value.js"
 import { EventEmitter } from "./Utils/EventEmitter.js"
 import { isValidProp } from "./Utils/isValidProp.js"
 import { loadState } from "./Utils/Store.js"
+import { Note } from "./Models/Note.js"
 
 class AppState extends EventEmitter {
-  /** @type {import('./Models/Value').Value[]} */
-  values = loadState('values', [Value])
+  /** @type {import('./Models/Note').Note[]} */
+  notes = []
 }
 
 export const appState = new Proxy(new AppState(), {
